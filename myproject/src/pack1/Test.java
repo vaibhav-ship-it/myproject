deeb750a689ee3d2f3c42104052d950e058d0c23
Vaibhav <vaibhav0320@gmail.com>
2021-03-06 11:50:18 +0530

    Creating remote branch master and committing files in it.

---
+++ myproject/src/pack1/Test.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package pack1;
+
+/**
+ * @author vaibhav
+ *
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World !!!");
+	}
+
+}
